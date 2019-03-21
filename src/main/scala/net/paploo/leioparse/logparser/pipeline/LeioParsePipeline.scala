98b6e0131e4d636bdec2993ec01223b5f83fc9a5
Jeff Reinecke <jeff@paploo.net>
2019-03-20 22:12:57 -0700

    Pipeline simplified thanks to Kleisli

--- src/main/scala/net/paploo/leioparse/logparser/pipeline/LeioParsePipeline.scala
+++ src/main/scala/net/paploo/leioparse/logparser/pipeline/LeioParsePipeline.scala
@@ -2,13 +2,11 @@ package net.paploo.leioparse.logparser.pipeline
 
 import java.nio.file.Path
 
-import cats.Functor
+import cats.data.Kleisli
 import cats.implicits._
 import net.paploo.leioparse.logparser.pipeline.LeioParsePipeline.DataDirectory
-import net.paploo.leioparse.util.extensions.Implicits._
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success, Try}
 
 trait LeioParsePipeline[A] extends (DataDirectory => Future[Seq[A]])
 
@@ -28,16 +26,8 @@ object LeioParsePipeline {
   }
 
   def apply[A](fileLocator: LeioFileLocator, reader: LeioReader, parser: LeioParser[A])(implicit ec: ExecutionContext): LeioParsePipeline[A] = {
-    val rowF: DataDirectory => Future[Seq[Row]] = fileLocator andThen reader
-    val parserF: Row => Try[A] = parser
-    val f1: Row => Future[A] = row => Future.fromTry(parser(row))
-    val f2: Seq[Row] => Future[Seq[A]] = rows => Future.sequence(rows.map(f1))
-    dataDirectory => rowF(dataDirectory) flatMap f2
-  }
-
-  private[this] def sequenceTry[A](seq: Seq[Try[A]]): Try[Seq[A]] = seq.foldLeft[Try[Seq[A]]](Success(Vector.empty[A])){
-    case (ts, Success(a)) => ts.map(_ :+ a)
-    case (ts, Failure(e)) => Failure(e)
+    val rowsParser: Seq[Row] => Future[Seq[A]] = rows => Future.sequence(rows.map(row => Future.fromTry(parser(row))))
+    dataDirectory => (Kleisli(fileLocator andThen reader) andThen Kleisli(rowsParser)).run(dataDirectory)
   }
 
 }
