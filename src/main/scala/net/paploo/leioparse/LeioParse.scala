1d67ecd13f48b416e19aa6a36ec4b062c760f3da
Jeff Reinecke <jeff@paploo.net>
2019-03-24 13:26:04 -0700

    Composable apps!

--- src/main/scala/net/paploo/leioparse/LeioParse.scala
+++ src/main/scala/net/paploo/leioparse/LeioParse.scala
@@ -3,14 +3,19 @@ package net.paploo.leioparse
 import java.nio.file.Paths
 import java.util.concurrent.TimeUnit
 
+import cats.implicits._
 import net.paploo.leioparse.app.{App, AppArgs, _}
+import net.paploo.leioparse.app.App.Result.ShowResult
 import net.paploo.leioparse.util.extensions.Implicits._
 import net.paploo.leioparse.util.extensions.LoggingExtensions.Logging
 
+import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.Duration
 import scala.util.{Failure, Success, Try}
 
 object LeioParse extends Logging {
+
+  implicit val appExecutionContext: ExecutionContext = ExecutionContext.global
 
   def main(implicit rawArgs: Array[String]): Unit = {
     (for {
@@ -21,7 +26,7 @@ object LeioParse extends Logging {
       runResult <- app.run(appArgs).toTry(appTimeout)
     } yield runResult) match {
       case Success(result) =>
-        result.log(r => s"LeioParse.main($rawArgs) --> \n\t $r")
+        result.log(r => s"LeioParse.main(${rawArgs.toList}) --> ${r.show}")
         System.exit(0)
       case Failure(throwable) =>
         throwable.log(th => s"LeioParse encountered a fatal error: $th")
@@ -31,8 +36,8 @@ object LeioParse extends Logging {
 
   def getConfig(implicit rawArgs: Array[String]): Try[LeioParseConfig] = Try(LeioParseConfig(timeoutSeconds = 60))
 
-  def getApp(implicit rawArgs: Array[String]): Try[App] =
-    Try(ParseOverlayAndLogApp)
+  def getApp(implicit rawArgs: Array[String]): Try[App[_]] =
+    Try(ParseOverlayAndLogApp andThenRun ParseLeioFilesAndLogApp)
   //Try(ParseLeioFilesAndLogApp)
 
   def getAppArgs(implicit rawArgs: Array[String]): Try[AppArgs] = Try(AppArgs(
--- src/main/scala/net/paploo/leioparse/app/App.scala
+++ src/main/scala/net/paploo/leioparse/app/App.scala
@@ -2,6 +2,7 @@ package net.paploo.leioparse.app
 
 import java.nio.file.Paths
 
+import cats.Show
 import net.paploo.leioparse.app.App.Result
 import net.paploo.leioparse.bookoverlayparser.BookOverlayParser
 import net.paploo.leioparse.data.leiofile.{LeioBook, LeioSession}
@@ -11,66 +12,64 @@ import net.paploo.leioparse.util.extensions.LoggingExtensions.Implicits._
 import net.paploo.leioparse.util.extensions.LoggingExtensions.Logging
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
 
-trait App[A] extends (AppArgs => Future[Result[A]]) {
-  def run(args: AppArgs): Future[Result[A]]
-  override def apply(args: AppArgs): Future[Result[A]] = run(args)
+trait App[+A] extends (AppArgs => ExecutionContext => Future[Result[A]]) {
+
+  def run(args: AppArgs)(implicit ec: ExecutionContext): Future[Result[A]]
+
+  override def apply(args: AppArgs): ExecutionContext => Future[Result[A]] = ec => run(args)(ec)
+
+  def andThenRun[A1 >: A](second: App[A1])(implicit ec: ExecutionContext): App[A1] = App.CompositeApp(this, second)
 }
 
 object App {
 
   /**
     * Encapsulates the result values of one or more apps.
+    *
+    * When apps are composed, each has a singular result stored as a single element in the sequence.
     */
-  case class Result[+A](toSeq: Try[Seq[A]]) {
-    def ++[A1 >: A](other: Result[A1]): Result[A1] = Result(for {
-      res1 <- toTry
-      res2 <- other.toTry
-    } yield res1 ++ res2)
+  trait Result[+A] {
+    def toSeq: Seq[A]
+    def ++[A1 >: A](other: Result[A1]): Result[A1]
   }
 
   object Result {
-    def pure[A](a: => A): Result[A] = Result(Try(List(a)))
-    def fromTry[A](t: Try[A]): Result[A] = Result(t.map(_ :: Nil))
+    def apply[A](a: A): Result[A] = ResultVector(Vector(a))
+
+    private case class ResultVector[+A](toSeq: Vector[A]) extends Result[A] {
+      override def ++[A1 >: A](other: Result[A1]): Result[A1] = ResultVector(toSeq ++ other.toSeq)
+    }
+
+    implicit object ShowResult extends Show[Result[_]] {
+      override def show(result: Result[_]): String = result.toSeq.mkString(s"Result(\n\t", ",\n\t", "\n)")
+    }
   }
 
-  trait FutureContext {
-    self: App[_] =>
-    implicit val executionContext: ExecutionContext = ExecutionContext.global
+  private[App] case class CompositeApp[+A](first: App[A], second: App[A]) extends App[A] {
+    override def run(args: AppArgs)(implicit ec: ExecutionContext): Future[Result[A]] = for {
+      result1 <- first.run(args)
+      result2 <- second.run(args)
+    } yield result1 ++ result2
   }
-
-  /**
-    * Same as:
-    * {{{
-    * args => (for {
-    *   result1 <- Kleisli(firstApp)
-    *   result2 <- Kleisli(secondApp)
-    * } yield result1 ++ result2).run(args)
-    * }}}
-    */
-  def compose[A](firstApp: App[A], secondApp: App[A])(implicit ec: ExecutionContext): App[A] = args => for {
-    result1 <- firstApp(args)
-    result2 <- secondApp(args)
-  } yield result1 ++ result2
 
 
 
 }
 
-object ParseOverlayAndLogApp extends App[Seq[BookOverlay]] with App.FutureContext with Logging {
+object ParseOverlayAndLogApp extends App[Seq[BookOverlay]] with Logging {
 
-  override def run(args: AppArgs):  Future[Result[Seq[BookOverlay]]] = {
+  override def run(args: AppArgs)(implicit ec: ExecutionContext):  Future[Result[Seq[BookOverlay]]] = {
 
     val fp = Paths.get(".", "books.json").normalize()
-    BookOverlayParser(fp).parse.log(identity).map(Result.pure)
+    BookOverlayParser(fp).parse.log(identity).map(Result.apply)
   }
 
 }
 
-object ParseLeioFilesAndLogApp extends App[Result[(Seq[LeioBook], Seq[LeioSession])]] with App.FutureContext with Logging {
+object ParseLeioFilesAndLogApp extends App[(Seq[LeioBook], Seq[LeioSession])] with Logging {
 
-  override def run(args: AppArgs): Future[Result[(Seq[LeioBook], Seq[LeioSession])]] = {
+  override def run(args: AppArgs)(implicit ec: ExecutionContext): Future[Result[(Seq[LeioBook], Seq[LeioSession])]] = {
     args.log(identity)
     val parser = LeioLogParser.forPath(args.dataDirPath)
     for {
@@ -78,7 +77,7 @@ object ParseLeioFilesAndLogApp extends App[Result[(Seq[LeioBook], Seq[LeioSessio
       sessions <- parser.parseSessions
       _ = books.log(_.mkString("\n"))
       _ = sessions.log(_.mkString("\n"))
-    } yield Result.pure( (books, sessions) )
+    } yield Result.apply( (books, sessions) )
   }.log(r => s"result = $r")
 
 }
