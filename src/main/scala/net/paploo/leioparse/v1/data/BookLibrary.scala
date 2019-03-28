dbb40bbc3c6df994bf027bbc841e2cfbd71c77f5
Jeff Reinecke <jeff@paploo.net>
2019-03-27 21:29:10 -0700

    Removed legacy (v1) code entirely, now that we can output the same output using v2

--- src/main/scala/net/paploo/leioparse/v1/data/BookLibrary.scala
+++
@@ -1,9 +0,0 @@
-package net.paploo.leioparse.v1.data
-
-case class BookLibrary(toSeq: Seq[Book]) {
-
-  def findById(id: Book.Id): Option[Book] = toSeq.find(_.id == id)
-
-  def findByTitle(title: Book.Title): Option[Book] = toSeq.find(_.title == title)
-
-}
--- src/main/scala/net/paploo/leioparse/v1/parser/book/BookRowOverlayData.scala
+++
@@ -1,3 +0,0 @@
-package net.paploo.leioparse.v1.parser.book
-
-case class BookRowOverlayData(bookId: Int, wordsPerPage: Option[Int])
